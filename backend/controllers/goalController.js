025ca382cc2290cf81bed8af6d12254b2244ddca
4meyDam1e <amey.damle@mail.utoronto.ca>
2023-05-16 01:17:16 -0400

    Added CRUD functionality with JWT Authentication for Users

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,12 +1,10 @@
 const asyncHandler = require("express-async-handler");
-
 const Goal = require("../models/goalModel");
 
 // @desc  Set goal
 // @route  POST /api/goals
 // @access  Private
 const setGoal = asyncHandler(async (req, res) => {
-    console.log(req.body);
     if (!req.body.text) {
         res.status(400);
         throw new Error("Please add a text field");
--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const { getGoals, setGoal, updateGoal, deleteGoal } = require("../controllers/goalController");
 
 router.route("/").post(setGoal).get(getGoals);
-
 router.route("/:id").put(updateGoal).delete(deleteGoal);
 
 module.exports = router;
